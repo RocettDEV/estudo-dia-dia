513643f393a99e0c096a098b171c89ab3a9bd0af
Gustavo Rocetti <gurocetti19@hotmail.com>
2025-03-21 18:47:19 +0000

    Finalizando formatadorCep

--- src/FormatadorCepExemplo.java
+++ src/FormatadorCepExemplo.java
@@ -1,10 +1,16 @@
 public class FormatadorCepExemplo {
     public static void main(String[] args) {
-        
+        try {
+            String cepFormatado = formatarCep("23765064"); 
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+            System.out.println("O cep formatado não corresponde a regras de negocio");
+        }
     }
-    static String FormatarCep (String cep) throws CepInvalidoException {
-        if(cep.length() != 8)
-        throw new CepInvalidoException();
+
+    static String formatarCep(String cep) throws CepInvalidoException { // Método corrigido
+        if (cep.length() != 8)
+            throw new CepInvalidoException();
 
         return "23.765.064";
     }
